fc200993d77cc96b186e4f008a22534b1d74bc39
Yaroslava Musiichuk <iaroslava1607@gmail.com>
2022-02-16 11:38:12 +0200

    add baseURL

--- nuxt.config.js
+++ nuxt.config.js
@@ -28,11 +28,11 @@ export default {
 
   // Modules: https://go.nuxtjs.dev/config-modules
 
-  modules: [],
+  modules: ["@nuxtjs/axios"],
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {},
-  // axios: {
-  //   baseURL: process.env.BASE_URL,
-  // },
+  axios: {
+    baseURL: "https://www.googleapis.com/books/v1",
+  },
 };
